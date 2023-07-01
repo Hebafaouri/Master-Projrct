a8a6bbbdae1801b767bc2256c5c922a0e804d73a
Hebafaouri <126398763+Hebafaouri@users.noreply.github.com>
2023-07-01 22:11:19 +0300

    first upload

---
+++ master-project/app/Http/Controllers/UserDashboardComponent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+class UserDashboardComponent extends Component
+{
+    public function render()
+    {
+        return view('Livewire.User.UserDashboardComponent');
+    }
+}
+
+
+
+
---
+++ master-project/app/Http/Livewire/HopComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HopComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.hop-component');
+    }
+}
